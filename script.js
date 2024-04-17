d89febadea0b19fda9df05983ecdf9d0fcf9fb2b
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-04-17 14:21:12 +0400

    Create: GameBoard module with board size setting and render function

--- script.js
+++ script.js
@@ -3,4 +3,28 @@
 const startButton = document.querySelector("#start-button");
 const restartButton = document.querySelector("#restart-button");
 
-const GameBoard = (function () {})();
+const GameBoard = (function () {
+  // Define gameboard size
+  const boardSize = 3 * 3;
+
+  //   create gameboard array and push elements to it
+  let gameboard = [];
+  for (let i = 0; i < boardSize; i++) {
+    gameboard.push("");
+  }
+
+  const renderGame = () => {
+    let boardHTML = [];
+
+    gameboard.forEach((el, i) => {
+      const boardDiv = document.createElement("div");
+      boardDiv.setAttribute("id", `board-${i}`);
+      boardDiv.innerHTML = `${el}`;
+      boardHTML.push(boardDiv);
+    });
+  };
+
+  return {
+    renderGame,
+  };
+})();
